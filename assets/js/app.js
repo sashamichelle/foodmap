a88da8b859b936d23912211fe340ad281c2efe8c
sashamichelle <sashaavilescalderon1999@gmail.com>
2017-12-13 16:22:41 -0300

    condicionando mis funciones de seleccion

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,15 +13,36 @@ $(function(){
 
 $('select#select-1').on('change',function(){
     var valor1 = $(this).val();
-    if (valor1==valor1) {
-    	$('.sexta-col').html("<img src='assets/img/img-5.jpg' alt='' />");
+    /*primer precio*/
+    if (valor1=="$1500-$3000") {
+    	$('.sexta-col').html("<img src='assets/img/img-5.jpg' alt='' />" + "<img src='assets/img/img-6.jpg' alt='' />" );
+    	$('.octava-col').html("<img src='assets/img/img-7.jpg' alt='' />" + "<img src='assets/img/img-8.jpg' alt='' />" );
+    }
+    /*segundo precio*/
+     if (valor1=="$3500-$5000") {
+     	$('.sexta-col').html("<img src='assets/img/img-9.jpg' alt='' />" + "<img src='assets/img/img-10.jpg' alt='' />" );
+    	$('.octava-col').html("<img src='assets/img/img-11.jpg' alt='' />" + "<img src='assets/img/img-12.jpg' alt='' />" );
     }
    
 });
 
 $('select#select-2').on('change',function(){
     var valor2 = $(this).val();
-    alert(valor2);
+    /*primer tipo de comida*/
+ if (valor2=="Vegetariana") {
+    	$('.sexta-col').html("<img src='assets/img/img-13.jpg' alt='' />" + "<img src='assets/img/img-14.jpg' alt='' />");
+    $('.octava-col').html("<img src='assets/img/img-15.jpg' alt='' />" + "<img src='assets/img/img-16.jpg' alt='' />");
+    }
+    /*segundo tipo de comida*/
+    if (valor2=="Carnes y pescados") {
+    	$('.sexta-col').html("<img src='assets/img/img-17.jpg' alt='' />" + "<img src='assets/img/img-18.jpg' alt='' />");
+    $('.octava-col').html("<img src='assets/img/img-19.jpg' alt='' />" + "<img src='assets/img/img-20.jpg' alt='' />");
+    }
+    /*tercer tipo de comida*/
+    if (valor2=="Sin especificar") {
+    	$('.sexta-col').html("<img src='assets/img/img-21.jpg' alt='' />" + "<img src='assets/img/img-22.jpg' alt='' />");
+    $('.octava-col').html("<img src='assets/img/img-23.jpg' alt='' />" + "<img src='assets/img/img-24.jpg' alt='' />");
+    }
 });
 
 $('select#select-3').on('change',function(){
